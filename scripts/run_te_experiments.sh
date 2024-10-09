84d8c4b05befce9e163577aa241d841b22cd161d
Miguel Ponce-de-Leon <miguel.ponce@bsc.es>
2024-10-09 10:56:32 +0200

    update .gitignore and bash script

--- scripts/run_te_experiments.sh
+++ scripts/run_te_experiments.sh
@@ -1,14 +1,19 @@
-W=14
-D=7
-E="round"
+W=28
+D=14
+E="standard"
 CPUS=8
 X="risk_ij"
 Y="new_cases_by_100k"
-
 LAYERS="TE_cnig_provincias TE_zon_bas_13 TE_abs_09"
+USE_ETE="true"
+RUN_CMD="python scripts/calculate_transfer_entropy.py"
 for l in $LAYERS;
 do
-    echo "Runnin on layer ${l} with parameters w:${W} d:${D} e:${E} x:${X} y:${Y}"
-    python scripts/calculate_transfer_entropy.py -w ${W} -d ${D} -e ${E} -c ${CPUS} -x ${X} -y ${Y} outputs/${l}
-    echo "============== DONE ===============\n"
+    echo
+    echo "============= TE on ${l} ==========="
+    CMD_PARAMS="-w ${W} -d ${D} -e ${E} -c ${CPUS} -x ${X} -y ${Y} --use_ete ${USE_ETE} "
+    echo "* Runnin on layer ${l} with parameters:"
+    echo "${CMD_PARAMS}" | sed 's/ \-/\n\-/g'
+    # ${RUN_CMD} ${CMD_PARAMS}  outputs/${l}
+    echo "============== DONE ==============="
 done
